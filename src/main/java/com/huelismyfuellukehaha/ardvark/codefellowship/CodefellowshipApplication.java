fc046ee382bcc37b1ba06cbfbe5e2c477f169824
Luke Chandler <lhchandler4@gmail.com>
2019-06-17 15:06:24 -0700

    initial commit

---
+++ src/main/java/com/huelismyfuellukehaha/ardvark/codefellowship/CodefellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.huelismyfuellukehaha.ardvark.codefellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefellowshipApplication.class, args);
+	}
+
+}
