f58f4b206fae4a4abbb18b65eaeb9f1370db4926
Luke Chandler <lhchandler4@gmail.com>
2019-06-19 13:18:54 -0700

    MVP

---
+++ src/test/java/com/huelismyfuellukehaha/ardvark/codefellowship/AppUserControllerTest.java
@@ -0,0 +1,43 @@
+package com.huelismyfuellukehaha.ardvark.codefellowship;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+
+
+public class AppUserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        mockMvc.perform(get("/login")).andExpect(content().string(containsString("Welcome to the Codefellowship!")));
+    }
+
+    @Test
+    public void testLogout() throws Exception {
+        mockMvc.perform(get("/codefellowshipOfTheRing")).andExpect(content().string(containsString("CodeFellowship of the Ring")));
+    }
+
+    @Test
+    public void testMyProfile() throws Exception {
+        mockMvc.perform(get("/myprofile")).andExpect(content().string(containsString("")));
+    }
+
+}
--- src/test/java/com/huelismyfuellukehaha/ardvark/codefellowship/CodefellowshipApplicationTests.java
+++ src/test/java/com/huelismyfuellukehaha/ardvark/codefellowship/CodefellowshipApplicationTests.java
@@ -2,15 +2,42 @@ package com.huelismyfuellukehaha.ardvark.codefellowship;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class CodefellowshipApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testCodefellowship_nologin() throws Exception {
+		File login = new ClassPathResource("templates/login.html").getFile();
+		String html = new String(Files.readAllBytes(login.toPath()));
+
+		this.mockMvc
+				.perform(get("/login"))
+				.andExpect(status().isOk());
+////				.andExpect(view().name("login"))
+////				.andExpect(content().string(html));
 	}
 
 }
